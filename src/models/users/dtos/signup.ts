5be483e1b59e92527aaca87001fa3db1c06fd1cc
YuriSamp <yurisamp123@gmail.com>
2024-01-30 21:52:18 -0300

    refactor : refactor signup schema

--- src/models/users/dtos/signup.ts
+++ src/models/users/dtos/signup.ts
@@ -2,9 +2,12 @@ import * as z from "zod";
 
 const signUpSchema = z
   .object({
+    role: z.union([z.literal("Corretor"), z.literal("Cliente")]),
     email: z.string().email("Invalid mail"),
+    name: z.string().min(3, "Name must have at least 3 digits"),
+    profilePicture: z.string().url("Profile picture must be an url"),
     password: z.string().min(8, "Password length lower than 8 digits"),
-    confirmPassword: z.string().min(8),
+    confirmPassword: z.string().min(8, "Password length lower than 8 digits"),
   })
   .refine((fields) => fields.password === fields.confirmPassword, {
     message: "Passwords don't match",
