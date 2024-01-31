d0b33c28cb350fdf3991403781b7db3cbbe97266
YuriSamp <yurisamp123@gmail.com>
2024-01-30 22:12:24 -0300

    add mongosse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 
 import { app } from "./config/koa";
+import { mongooseConnect } from "./database/mongosse";
+
+const mongo = process.env.DATABASE_URL || "";
+
+mongooseConnect(mongo);
 
 app.listen(8000, () => {
   console.log(`server init on port ${8000}`);
